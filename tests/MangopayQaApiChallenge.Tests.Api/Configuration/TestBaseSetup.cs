5ab8f7723dee5d2f9251cb5676950c257b83ca2a
Filip Szymaniak <2039165@cognizant.com>
2024-09-07 20:15:05 +0200

    add TestBaseSetup.cs with appsettings.json

---
+++ tests/MangopayQaApiChallenge.Tests.Api/Configuration/TestBaseSetup.cs
@@ -0,0 +1,27 @@
+﻿using MangoPay.SDK;
+using Microsoft.Extensions.Configuration;
+
+namespace MangopayQaApiChallenge.Tests.Api.Configuration;
+
+public class TestBaseSetup
+{
+    protected readonly MangoPayApi Api;
+    private readonly AppSettings _appSettings;
+
+    public TestBaseSetup(MangoPayApi api)
+    {
+        var config = new ConfigurationBuilder()
+            .AddJsonFile("appSettings.json", optional: false)
+            .Build();
+
+        _appSettings = config.Get<AppSettings>()!;
+        Api = api;
+    }
+    
+    [OneTimeSetUp]
+    public void Setup()
+    {
+        Api.Config.ClientId = _appSettings.ClientId;
+        Api.Config.ClientPassword = _appSettings.ClientPassword;
+    }
+}
