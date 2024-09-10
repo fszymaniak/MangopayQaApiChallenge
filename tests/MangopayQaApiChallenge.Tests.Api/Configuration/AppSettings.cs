52539142751e300fba566b97187cd696019be898
fszymaniak <szymaniak.filip@gmail.com>
2024-09-10 13:14:37 +0200

    add TestBaseSetup.cs with appsettings.json

---
+++ tests/MangopayQaApiChallenge.Tests.Api/Configuration/AppSettings.cs
@@ -0,0 +1,7 @@
+﻿namespace MangopayQaApiChallenge.Tests.Api.Configuration;
+
+internal class AppSettings
+{
+    public string ClientId { get; set; }
+    public string ClientPassword { get; set; }
+}
