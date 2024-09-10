7cadcb10c76aecde1311393f0d3076b120dddf46
fszymaniak <szymaniak.filip@gmail.com>
2024-09-10 13:14:38 +0200

    add invalid input data test cases

--- tests/MangopayQaApiChallenge.Tests.Api/Tests/UnhappyPaths/InvalidInputs/CreateUserInvalidInputsTests.cs
+++ tests/MangopayQaApiChallenge.Tests.Api/Tests/UnhappyPaths/InvalidInputs/CreateUserInvalidInputsTests.cs
@@ -1,6 +1,33 @@
 ﻿namespace MangopayQaApiChallenge.Tests.Api.Tests.UnhappyPaths.InvalidInputs;
 
-public class CreateUserInvalidInputsTests
+public class CreateUserInvalidInputsTests : TestBaseSetup
 {
+    public CreateUserInvalidInputsTests() : base(new MangoPayApi())
+    {
+    }
     
+    [Test]
+    [AllureLabel("AcceptanceCriteria", "AC02")]
+    [AllureLabel("TestCase", "TC05")]
+    public async Task UserEndpoint_TryToCreateUserWithInvalidEmail_BadRequest()
+    {
+        UserDTO response = null!;
+        var invalidEmail = "email_invalid@";
+        var userRequestData = UserFactory.CreateValidUser();
+        userRequestData.Email = invalidEmail;
+        
+        try
+        {
+            response = await Api.Users.CreatePayerAsync(userRequestData);
+        }
+        catch (Exception exception)
+        {
+            exception.GetType().ShouldBe(typeof(ResponseException));
+            exception.Message.ShouldContain($"The field Email must match the regular expression");
+        }
+        
+        response.ShouldBe(null);
+        
+        await StatusCodeValidator.ValidateStatusCode400BadRequest();
+    }
 }
